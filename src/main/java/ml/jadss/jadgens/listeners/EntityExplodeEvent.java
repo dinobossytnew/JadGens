67fd6e6ee34f163f8d5cb1357a911df5023e2b46
JadssDev <jadss.pt.com.pt@gmail.com>
2020-09-12 16:30:53 +0100

    Changed for full compatibility with 1.7

--- src/main/java/ml/jadss/jadgens/listeners/EntityExplodeEvent.java
+++ src/main/java/ml/jadss/jadgens/listeners/EntityExplodeEvent.java
@@ -1,18 +1,15 @@
 package ml.jadss.jadgens.listeners;
 
 import ml.jadss.jadgens.JadGens;
-import ml.jadss.jadgens.utils.Machine;
 import ml.jadss.jadgens.utils.MachineLookup;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockExplodeEvent;
-import org.bukkit.event.entity.EntityExplodeEvent;
 
-public class BlockExplodeListener implements Listener {
+public class EntityExplodeEvent implements Listener {
 
     @EventHandler
-    public void blockExplodeEvent(BlockExplodeEvent e) {
+    public void entityExplodeEvent(org.bukkit.event.entity.EntityExplodeEvent e) {
         if (!JadGens.getInstance().getConfig().getBoolean("machinesConfig.preventMachineExplosion")) return;
         MachineLookup checker = new MachineLookup();
         for (Block b : e.blockList()) {
